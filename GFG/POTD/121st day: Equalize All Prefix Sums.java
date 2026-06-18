791b1bc572e6f39898ed6a66c70214c15971874f
Shruti Jha <shrutijha1105@gmail.com>
2026-06-19 00:11:16 +0530

    Implement optimalArray method for prefix sum equalization

---
+++ GFG/POTD/121st day: Equalize All Prefix Sums.java
@@ -0,0 +1,27 @@
+class Solution {
+	public ArrayList<Integer> optimalArray(int[] arr) {
+		// code here
+		int n = arr.length;
+		ArrayList<Integer> ans = new ArrayList<>();
+		
+		long[] pref = new long[n + 1];
+		for (int i = 0; i < n; i++) {
+			pref[i + 1] = pref[i] + arr[i];
+		}
+		
+		for (int i = 0; i < n; i++) {
+			int mid = i / 2;
+			long median = arr[mid];
+			
+			long leftSum = pref[mid];
+			long rightSum = pref[i + 1] - pref[mid + 1];
+			
+			long leftCost = median * mid - leftSum;
+			long rightCost = rightSum - median * (i - mid);
+			
+			ans.add((int)(leftCost + rightCost));
+		}
+		
+		return ans;
+	}
+}
