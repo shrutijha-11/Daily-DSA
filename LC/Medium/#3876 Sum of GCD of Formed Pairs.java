66deafe272fbfda1da8cef03b56543960d2f5ecc
Shruti Jha <shrutijha1105@gmail.com>
2026-07-16 15:45:51 +0530

    Create #3876 Sum of GCD of Formed Pairs.java

---
+++ LC/Medium/#3876 Sum of GCD of Formed Pairs.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            mx = Math.max(mx, nums[i]);
+            prefixGcd[i] = gcd(nums[i], mx);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long ans = 0;
+        for (int i = 0; i < n / 2; i++) {
+            ans += gcd(prefixGcd[i], prefixGcd[n - 1 - i]);
+        }
+
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
